4e6dd52222ae59f7c3f99b8771ab544af239e1c6
Gerard Rey <gerard@echiverri.net>
2025-10-14 16:20:27 -0700

    feat: version 0.1.1

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,7 @@
 import { Logo } from '@/app/logo'
 
 import type { Metadata } from 'next'
+import { Suspense } from 'react'
 import ClientComponent from './client-component'
 
 export const metadata: Metadata = {
@@ -11,7 +12,9 @@ export default function Login() {
   return (
     <div className="w-full max-w-xs">
       <Logo size="sm" data-logo-only />
-      <ClientComponent />
+      <Suspense fallback={<>...</>}>
+        <ClientComponent />
+      </Suspense>
     </div>
   )
 }
