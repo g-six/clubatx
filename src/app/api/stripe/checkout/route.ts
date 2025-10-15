7b094e8ada692d925a484a7fef0771ded6857f19
Gerard Rey <gerard@echiverri.net>
2025-10-14 21:22:58 -0700

    feat: fix stripe hook

--- src/app/api/stripe/checkout/route.ts
+++ src/app/api/stripe/checkout/route.ts
@@ -13,7 +13,7 @@ export async function POST(req: NextRequest) {
   let recorded = await supabase
     .from('bookings')
     .update({
-      status: 'PAYING',
+      status: object.payment_status?.toUpperCase() || 'PENDING',
     })
     .eq('id', object.client_reference_id)
     .select()
@@ -22,35 +22,36 @@ export async function POST(req: NextRequest) {
     // Get the signature sent by Stripe
     const signature = req.headers.get('stripe-signature')
     if (signature) {
+      console.log({ signature })
       try {
         const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
-        const event = stripe.webhooks.constructEvent(payload, signature, process.env.STRIPE_DEFAULT_CHECKOUT_SECRET)
+        // const event = stripe.webhooks.constructEvent(payload, signature, process.env.STRIPE_DEFAULT_CHECKOUT_SECRET)
 
-        // Handle the event
-        switch (event.type) {
-          case 'payment_intent.succeeded':
-            const paymentIntent = event.data.object
-            console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`)
-            recorded = await supabase
-              .from('bookings')
-              .update({
-                status: 'PAID',
-              })
-              .eq('id', object.client_reference_id)
-              .select()
-            booking = recorded?.data?.pop()
-            // Then define and call a method to handle the successful payment intent.
-            // handlePaymentIntentSucceeded(paymentIntent);
-            break
-          case 'payment_method.attached':
-            const paymentMethod = event.data.object
-            // Then define and call a method to handle the successful attachment of a PaymentMethod.
-            // handlePaymentMethodAttached(paymentMethod);
-            break
-          default:
-            // Unexpected event type
-            console.log(`Unhandled event type ${event.type}.`)
-        }
+        // // Handle the event
+        // switch (event.type) {
+        //   case 'payment_intent.succeeded':
+        //     const paymentIntent = event.data.object
+        //     console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`)
+        //     recorded = await supabase
+        //       .from('bookings')
+        //       .update({
+        //         status: 'PAID',
+        //       })
+        //       .eq('id', object.client_reference_id)
+        //       .select()
+        //     booking = recorded?.data?.pop()
+        //     // Then define and call a method to handle the successful payment intent.
+        //     // handlePaymentIntentSucceeded(paymentIntent);
+        //     break
+        //   case 'payment_method.attached':
+        //     const paymentMethod = event.data.object
+        //     // Then define and call a method to handle the successful attachment of a PaymentMethod.
+        //     // handlePaymentMethodAttached(paymentMethod);
+        //     break
+        //   default:
+        //     // Unexpected event type
+        //     console.log(`Unhandled event type ${event.type}.`)
+        // }
       } catch (err: any) {
         console.log(`⚠️  Webhook signature verification failed.`, err.message)
         return NextResponse.json(err, {
